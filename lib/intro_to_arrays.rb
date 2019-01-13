f0a17c3f5cfa85653052c5eb3281dba890390a10
amusing-bolt-9198 <edmund.nelson.b@gmail.com>
2019-01-13 00:47:52 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,24 @@
+def instantiate_new_array
+  return []
+end
+def array_with_two_elements
+  return [2, 2]
+end
+def first_element (arr)
+  return arr[0]
+end
+def third_element(arr)
+  return arr[2]
+end
+def last_element(arr)
+  return arr[-1]
+end
+def first_element_with_array_methods(arr)
+  return arr.shift
+end
+def last_element_with_array_methods(arr)
+  return arr.pop
+end
+def length_of_array(arr)
+  return arr.size 
+end
